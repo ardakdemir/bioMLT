ed77676ad6c5b6daa77c45a0af01aca466ea2b32
Arda Akdemir <ardaakdemir1@gmail.com>
2020-06-16 20:54:53 +0900

    bos isler

--- qasonly_submit.sh
+++ qasonly_submit.sh
@@ -3,7 +3,7 @@ output_dir='qasonly_1606'
 epoch_num=30
 #$ -cwd
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
-#$ -N ner_only_train
+#$ -N qas_only_train
 cd ~
 singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir qas_experiments  --load_model --max_seq_length 512 --num_train_epochs 50
 
