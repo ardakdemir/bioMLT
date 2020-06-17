23626e3a951793155f6d4d36d46f4e67bede0257
Arda Akdemir <ardaakdemir1@gmail.com>
2020-06-17 20:40:27 +0900

    joint_flat_added

--- joint_flat_submit.sh
+++ joint_flat_submit.sh
@@ -4,6 +4,6 @@ output_dir='joint_1706'
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
 #$ -N joint_flat_train
 cd ~
-singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode joint_flat --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs 50
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode joint_flat --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs 20
 
 echo "All results are stored in "$output_dir
