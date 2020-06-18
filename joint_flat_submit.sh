e28f8e11aeb225e94ff9d952ed50b4f5337442d6
Arda Akdemir <ardaakdemir1@gmail.com>
2020-06-18 19:34:55 +0900

    joint_30_epoch

--- joint_flat_submit.sh
+++ joint_flat_submit.sh
@@ -20,7 +20,7 @@ do
         output_dir=${output_dir_temp}"_"$file
         echo "output directory : "$output_dir
         save_name="best_model_"${file}
-        singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode joint_flat  --model_save_name ${save_name}--ner_train_file $train_file --ner_dev_file $dev_file  --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs $epoch
+        singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode joint_flat  --model_save_name ${save_name} --ner_train_file $train_file --ner_dev_file $dev_file  --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs $epoch
     fi
 done
 
