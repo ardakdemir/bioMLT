f26b21c5a331cff0dc8538ab2febbcad7c6c4a63
Arda Akdemir <ardaakdemir1@gmail.com>
2020-06-18 11:02:26 +0900

    joint_flat_for_all

--- joint_flat_submit.sh
+++ joint_flat_submit.sh
@@ -1,7 +1,7 @@
 /usr/local/bin/nosh
-output_dir='joint_1706'
-output_dir_temp='joint_1706'
-
+output_dir_temp=${1}
+epoch=5
+baseline_model_path='biobert-squadv1.1_pretrained'
 #$ -cwd
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
 #$ -N joint_flat_train
@@ -20,7 +20,7 @@ do
         echo $dev_file
         output_dir=output_dir_temp"_"$file
         echo "output directory : "$output_dir
-        singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode joint_flat  --ner_train_file $train_file --ner_dev_file $dev_file  --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs 20
+        singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --biobert_model_path ${baseline_model_path} --mode joint_flat  --ner_train_file $train_file --ner_dev_file $dev_file  --output_dir $output_dir  --load_model --max_seq_length 256 --num_train_epochs $epoch
     fi
 done
 
