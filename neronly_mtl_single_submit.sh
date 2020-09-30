2b3f8df5243433319c8f8fd11159a7915dc43f47
Arda Akdemir <ardaakdemir1@gmail.com>
2020-10-01 07:59:37 +0900

    submit mtl as separate tasks

---
+++ neronly_mtl_single_submit.sh
@@ -0,0 +1,24 @@
+/usr/local/bin/nosh
+output_dir=$1
+target_dataset=$2
+aux_dataset=$3
+epoch_num=20
+total_train_steps=1000
+#$ -cwd
+#$ -l v100=1,s_vmem=100G,mem_req=100G
+cd ~
+ner_data_folder="biobert_data/datasets/NER"
+datasets=$(ls ${ner_data_folder})
+# Fix the dataset for now
+
+folder_path=$ner_data_folder"/"$target_dataset
+aux_folder_path=$ner_data_folder"/"$aux_dataset
+target_train_file=$folder_path"/ent_train.tsv"
+target_test_file=$folder_path"/ent_test.tsv"
+target_dev_file=$folder_path"/ent_devel.tsv"
+aux_train_file=$aux_folder_path"/ent_train.tsv"
+aux_test_file=$aux_folder_path"/ent_test.tsv"
+aux_dev_file=$aux_folder_path"/ent_devel.tsv"
+
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --crf --mode multiner --total_train_steps $total_train_steps --output_dir $output_dir  --num_train_epochs $epoch_num --ner_train_files  $target_train_file  $aux_train_file --ner_dev_files $target_dev_file $aux_dev_file  --ner_test_files $target_test_file $aux_test_file  --load_model
+echo "Results are stored in "$output_dir
--- submitter.sh
+++ submitter.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
-qsub -N submitted_from_cli neronly_multi_submit.sh mtl_submitter_deneme_0110
+output_dir=$1
+if [ -d $output_dir ]
+then
+  rm $output_dir
+fi
+
+pref_name="cli_multitasks_0110"
+
+ner_data_folder="biobert_data/datasets/NER"
+datasets=$(ls ${ner_data_folder})
+# Fix the dataset for now
+target=0
+for target_fd in $datasets
+do
+    folder_path=$ner_data_folder"/"$target_fd
+    if [ -d $folder_path ]
+    echo "Iteration for "$folder_path
+    then
+        aux=0
+        for aux_fd in $datasets
+        do
+            aux_folder_path=$ner_data_folder"/"$aux_fd
+            if [ -d $aux_folder_path ]
+            then
+                if [ $target -le $aux ]
+                then
+                  qsub -N $pref_name"_target_"$target_fd"_aux_"$aux_fd neronly_mtl_single_submit.sh $output_dir $target_fd $aux_fd
+               fi
+            fi
+
+            aux=$(($aux + 1))
+        done
+    fi
+    target=$(($target + 1))
+done
+
