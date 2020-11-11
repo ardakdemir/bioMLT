387674470fc262d8f6bcf54eeddc1fd1cb5d9bdf
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-11 22:21:15 +0900

    remove torchsummary

--- squad_metrics.py
+++ squad_metrics.py
@@ -80,7 +80,7 @@ def get_raw_scores(examples, preds):
         try:
             gold_answers = [answer["text"] for answer in example.answers if normalize_answer(answer["text"])]
         except:
-            gold_answers = [answer for answer in example.answers["text"] if normalize_answer(answer)]
+            gold_answers = [answer for answer in example.answers[0]["text"] if normalize_answer(answer)]
 
         if not gold_answers:
             # For unanswerable questions, only correct answer is empty string
