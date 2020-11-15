b8b290d731dc4ecace781637b255f8e74aa83ae8
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-15 13:50:37 +0900

    remove torchsummary

--- qashier_submit.sh
+++ qashier_submit.sh
@@ -5,9 +5,9 @@ ner_dataset_name=${3}
 
 #$ -cwd
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
-#$ -N qas_only_train
+#$ -N qas_hier_train
 
-load_model_path=${ner_folder}"/"${ner_dataset_name}"/best_ner_model_on_"${ner_dataset_name}
+load_model_path=${ner_folder}"/best_ner_model_on_"${ner_dataset_name}
 load_ner_vocab_path=${load_model_path}"_vocab"
 
 singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model_path ${load_model_path} --load_ner_label_vocab_path ${load_ner_vocab_path} --max_seq_length 256 --num_train_epochs 30
