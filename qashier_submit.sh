e42cadd06b98ac304059319c510db4cb1601ba86
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-15 13:54:28 +0900

    remove torchsummary

--- qashier_submit.sh
+++ qashier_submit.sh
@@ -10,4 +10,4 @@ ner_dataset_name=${3}
 load_model_path=${ner_folder}"/best_ner_model_on_"${ner_dataset_name}
 load_ner_vocab_path=${load_model_path}"_vocab"
 
-singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model_path ${load_model_path} --load_ner_label_vocab_path ${load_ner_vocab_path} --max_seq_length 256 --num_train_epochs 30
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --mode qas --output_dir $output_dir  --load_model_path ${load_model_path} --load_ner_label_vocab_path ${load_ner_vocab_path} --max_seq_length 256 --num_train_epochs 30  --qas_with_ner --init_ner_head --crf
