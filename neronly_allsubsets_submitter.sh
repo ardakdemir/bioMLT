42d97c8f761b72cc7abf1fc8cf0d21a71f5dddee
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-20 19:17:37 +0900

    remove torchsummary

--- neronly_allsubsets_submitter.sh
+++ neronly_allsubsets_submitter.sh
@@ -8,17 +8,18 @@ then
 fi
 
 ner_data_folder_pref="/home/aakdemir/biobert_data/datasets/subsetNER_for_QAS_1000_*"
+root_folder="/home/aakdemir/"
 dataset_folders=$(ls ${ner_data_folder_pref})
 # Fix the dataset for now
 for subset_folder in $dataset_folders
 do
     echo $subset_folder
-    subset_folder_path=$ner_data_folder_pref "/" $subset_folder
+    subset_folder_path=$root_folder "/" $subset_folder
     datasets=$(ls ${ner_data_folder_pref})
     for target_fd in $datasets
     do
       folder_path=$subset_folder_path"/"$target_fd
-      echo "NER FOLDER "$folder_path
+      echo "Dataset path "$folder_path
     done
 done
 
