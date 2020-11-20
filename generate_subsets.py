62bc29db89edbfbf6688a150408d176c161c37b6
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-20 19:04:35 +0900

    remove torchsummary

---
+++ generate_subsets.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import subprocess
+
+file_names = ["ent_train.tsv", "ent_devel.tsv", "ent_test.tsv"]
+
+
+def get_small_dataset(file, shrink=None, size=None):
+    limit = size
+    sentences = open(file).read().split("\n\n")[:-1]
+    if shrink is not None:
+        limit = int(len(sentences) / shrink)
+    if shrink is None and size is None:
+        limit = len(sentences)
+    np.random.shuffle(sentences)
+    x = sentences[:limit]
+    return "\n\n".join([sent for sent in x])
+
+
+def generate_small_datasets(folder, save_folder, shrink=None, size=None):
+    datasets = os.listdir(folder)
+    for dataset in datasets:
+        path = os.path.join(folder, dataset)
+        if not os.path.isdir(path) or any([f not in os.listdir(path) for f in file_names]):
+            print("Skipping {} ".format(dataset))
+        dataset_save_folder = os.path.join(save_folder, dataset)
+        if not os.path.exists(dataset_save_folder):
+            os.makedirs(dataset_save_folder)
+        for file in file_names:
+            file_path = os.path.join(path, file)
+            save_path = os.path.join(dataset_save_folder, file)
+            if file == "ent_test.tsv":
+                cmd = "cp {} {}".format(file_path, save_path)
+                subprocess.call(cmd, shell=True)
+            else:
+                shrinked = get_small_dataset(file_path, shrink=shrink, size=size)
+                with open(save_path, "w") as s_p:
+                    s_p.write(shrinked)
+
+
+subset_sizes = [1000,2000,3000,4000,5000,None]
+repeat = 5
+root_folder = "biobert_data/datasets/NER_for_QAS"
+save_folder_prefix = "biobert_data/datasets/subsetNER_for_QAS_"
+for size in subset_sizes:
+    for r in range(repeat):
+        save_name = save_folder_prefix + "{}_".format(str(size) if size is not None else "All")
+        save_name = save_name + str(r)
+        generate_small_datasets(root_folder,save_name,size = size)
+        if size is None:
+            break
--- qashier_submit.sh
+++ qashier_submit.sh
@@ -10,4 +10,4 @@ ner_dataset_name=${3}
 load_model_path=${ner_folder}"/best_ner_model_on_"${ner_dataset_name}
 load_ner_vocab_path=${load_model_path}"_vocab"
 
-singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --ner_dataset_name ${ner_dataset_name} --mode qas --output_dir $output_dir  --load_model_path ${load_model_path} --load_ner_label_vocab_path ${load_ner_vocab_path} --max_seq_length 256 --num_train_epochs 6  --qas_with_ner --init_ner_head --crf --model_save_name best_qas_model_${ner_dataset_name}
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --ner_dataset_name ${ner_dataset_name} --mode qas --output_dir $output_dir  --load_model_path ${load_model_path} --load_ner_label_vocab_path ${load_ner_vocab_path} --max_seq_length 256 --num_train_epochs 10  --qas_with_ner --init_ner_head --crf --model_save_name best_qas_model_${ner_dataset_name}
