735771759c458447cadd9f020519bb83061a9065
Arda Akdemir <ardaakdemir1@gmail.com>
2020-11-23 18:24:53 +0900

    remove torchsummary

--- qashier_all_multiplefolders_submitter.sh
+++ qashier_all_multiplefolders_submitter.sh
@@ -2,19 +2,23 @@
 
 output_dir_pref=$1
 ner_datasets_pref=$2
-exp_name=$3
+ner_folders_pref=$3
+exp_name=$4
 
 l=$(expr length "${ner_datasets_pref}")
 echo $l
 
-ner_folders=$(ls -d ${ner_datasets_pref}*)
+ner_dataset_folders=$(ls -d ${ner_datasets_pref}*)
+ner_folders=$(ls -d ${ner_folders_pref}*)
 # Fix the dataset for now
 for folder in $ner_folders
 do
   exp_code=${folder:$l}
-  datasets=$(ls ${folder})
   ner_folder=$ner_datasets_pref"_"$exp_code
+  echo "NER dataset folder: "$ner_folder
   output_dir=$output_dir_pref"_"$exp_code
+  datasets=$(ls ${ner_folder})
+  echo "All datasets: "$datasets
   if [ -d $output_dir ]
   then
     rm $output_dir
@@ -23,8 +27,8 @@ do
   do
       echo "Submitting "${target_fd}
       echo "Ner  dir: "$ner_folder
-      echo "Output dir"$output_dir
-      echo "Target dataset"$target_fd
+      echo "Output dir: "$output_dir
+      echo "Target dataset: "$target_fd
       #qsub -N $pref_name"_nermodel_"$target_fd /home/aakdemir/bioMLT/qashier_submit.sh $output_dir $ner_folder $target_fd
   done
 done
