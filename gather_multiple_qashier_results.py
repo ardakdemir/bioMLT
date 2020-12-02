c0699ba73ab005fe7e8093e089c1fbbdd62931bc
Arda Akdemir <ardaakdemir1@gmail.com>
2020-12-02 11:09:26 +0900

    generate_subsets is changed

--- gather_multiple_qashier_results.py
+++ gather_multiple_qashier_results.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 
 def get_single_result(result_folder, file_name="qas_latex_table", offset=2):
     file_path = os.path.join(result_folder, file_name)
@@ -46,7 +46,10 @@ def get_multiple_results(root, folder_pref):
             results[folder_exp_name] = result
     return results
 # results = get_single_result(".")
-folder_pref = "qashiers_"
+
+args = sys.argv
+
+folder_pref = args[1]
 results_dict = get_multiple_results(".", folder_pref)
 results_table = printDict(results_dict)
 print(results_table)
