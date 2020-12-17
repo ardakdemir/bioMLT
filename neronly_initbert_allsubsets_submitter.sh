1b03ea2a8702eff025fb0c4cc0347c347670b934
Arda Akdemir <ardaakdemir1@gmail.com>
2020-12-17 15:23:28 +0900

    generate_subsets is changed

--- neronly_initbert_allsubsets_submitter.sh
+++ neronly_initbert_allsubsets_submitter.sh
@@ -3,10 +3,7 @@
 output_pref=$1
 ner_folder_pref=$2
 pref_name=$3
-if [ -d $output_dir ]
-then
-  rm $output_dir
-fi
+
 pref_length=$(expr length $ner_folder_pref)
 ner_data_folder_pref=$ner_folder_pref
 ner_data_folder_pref=$ner_data_folder_pref*
@@ -22,15 +19,20 @@ do
     exp_code=${subset_folder:$pref_length}
     output_dir=$output_pref"_"$exp_code
     y=0
-    echo "Output dir: "$output_dir
+    echo "NER Output dir: "$output_dir
+    echo "QAS Output dir: "qas_output_dir
     for target_fd in $datasets
     do
       folder_path=$subset_folder_path"/"$target_fd
       echo "Dataset path "$folder_path
+      echo "Target dataset: "$target_fd
       y=$((y + 1))
-      experiment_name=${pref_name}"_"${exp_code}
-      echo "Experiment name: "$experiment_name
-      qsub -N $experiment_name /home/aakdemir/bioMLT/neronly_initbert_stl_submit.sh $output_dir $subset_folder_path $target_fd
+      ner_experiment_name="NER_"${pref_name}"_"${exp_code}
+      qas_experiment_name="QAS_"${pref_name}"_"${exp_code}
+      echo "NER Experiment name: "$ner_experiment_name
+      qsub -N $ner_experiment_name /home/aakdemir/bioMLT/neronly_initbert_stl_submit.sh $output_dir $subset_folder_path $target_fd
+      echo "QAS Experiment name: "$qas_experiment_name
+      qsub -N $qas_experiment_name /home/aakdemir/bioMLT/qashier_submit.sh $output_dir $output_dir $target_fd
     done
     x=$((x + 1))
 done
