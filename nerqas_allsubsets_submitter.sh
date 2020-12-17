2796dd84222c2c50754e036780ba7a2e97415570
Arda Akdemir <ardaakdemir1@gmail.com>
2020-12-17 15:57:58 +0900

    generate_subsets is changed

--- nerqas_allsubsets_submitter.sh
+++ nerqas_allsubsets_submitter.sh
@@ -23,7 +23,6 @@ do
     echo "NER Output dir: "$output_dir
     for target_fd in $datasets
     do
-      folder_path=$subset_folder_path"/"$target_fd
       echo "Dataset path "$folder_path
       echo "Target dataset: "$target_fd
       y=$((y + 1))
