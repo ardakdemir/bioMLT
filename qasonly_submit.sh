fbcd38149ba1710e34526d3dcd44fccbe53a383a
Arda Akdemir <ardaakdemir1@gmail.com>
2020-12-20 08:56:36 +0900

    generate_subsets is changed

--- qasonly_submit.sh
+++ qasonly_submit.sh
@@ -5,12 +5,5 @@ load_model_path=${2}
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
 #$ -N qas_only_train
 cd ~
-if [ -f $load_model_path ]
-then
-  echo "LOADING PRETRAINED MODEL "${load_model_path}
-  singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model_path ${load_model_path} --max_seq_length 256
-else
-  echo "NOT LOADING MODEL. STARTING WITH Biobert v1.1"
-  singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model --load_model
-fi
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model --load_model
 echo "All results are stored in "$output_dir
