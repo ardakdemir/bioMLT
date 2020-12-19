202ace2f6d1e57a1a8e48197a1169ba44e72a301
Arda Akdemir <ardaakdemir1@gmail.com>
2020-12-20 08:50:35 +0900

    generate_subsets is changed

--- qasonly_submit.sh
+++ qasonly_submit.sh
@@ -11,6 +11,6 @@ then
   singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model_path ${load_model_path} --max_seq_length 256
 else
   echo "NOT LOADING MODEL. STARTING WITH Biobert v1.1"
-  singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model --max_seq_length 256   --batch_size 12 --load_model
+  singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --output_dir $output_dir  --load_model --load_model
 fi
 echo "All results are stored in "$output_dir
