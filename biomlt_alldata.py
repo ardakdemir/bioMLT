883d395b32f24824cad63f134c2d7471c2534c39
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-03 21:28:28 +0900

    generate_subsets is changed

--- biomlt_alldata.py
+++ biomlt_alldata.py
@@ -1201,8 +1201,6 @@ class BioMLT(nn.Module):
                 return all_preds, loss
         # logging.info("NER output {} ".format(ner_outs.))
         else:
-            print("NER INDS")
-            print(ner_inds)
             ner_outs = ner_head(bert_hiddens, ner_inds)
             return ner_outs
 
