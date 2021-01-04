27caf2daa1b39a0db6dd810312a3a33ac18800db
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-04 16:56:32 +0900

    sh file is changed

--- tsne_generation.py
+++ tsne_generation.py
@@ -12,7 +12,6 @@ import numpy as np
 from sklearn.manifold import TSNE
 
 
-
 def parse_args():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Working  on {}".format(device))
@@ -20,31 +19,52 @@ def parse_args():
     parser.add_argument('--ner_vector_file', type=str, default='bert_vectors/All-entities.hdf5')
     parser.add_argument('--qas_vector_file', type=str, default='bert_vectors/BioASQ-training8b.hdf5')
     parser.add_argument('--save_folder', type=str, default='tsne_vectors')
+    parser.add_argument('--patience', type=int, default=100, help="Number of epochs without progress")
+    parser.add_argument('--n_iter', type=int, default=500, help="Number of epochs ")
+    parser.add_argument('--tsne_dim', type=int, default=2, help="Number of dims ")
+    parser.add_argument('--limit', type=int, default=50000, help="Number of vectors from each dataset.")
+
     args = parser.parse_args()
     args.device = device
     return args
 
-#Get data
+
+
+# Get data
 def get_stored_features(file_name):
-    with h5py.File(file_name,"r") as h:
+    with h5py.File(file_name, "r") as h:
         feats = h["vectors"][:]
-    feats = feats.reshape(-1,768)
+    feats = feats.reshape(-1, 768)
     return feats
 
-def tsne_generation(vectors):
+
+def tsne_generation(vectors, args):
+    patience = args.patience
+    n_iter = args.n_iter
+    tsne_dim = args.tsne_dim
     b = time.time()
-    tsne = TSNE(n_components=2, n_iter=250, n_iter_without_progress=50)
+    tsne = TSNE(n_components=tsne_dim, n_iter=n_iter, n_iter_without_progress=patience)
     tsne_vectors = tsne.fit_transform(vectors)
     e = time.time()
     t = round(e - b, 3)
-    print("{} tSNE are generated in {} seconds".format(len(tsne_vectors),t))
+    print("{} tSNE are generated in {} seconds".format(len(tsne_vectors), t))
     return tsne_vectors
+
+
+def plot_visualization(vector_array,names,save_path):
+    plt.figure()
+    for vecs,name in zip(vector_array,names):
+        plt.scatter(vecs[:, 0], vecs[:, 1], label=name, zorder=0)
+    plt.legend()
+    plt.savefig(save_path)
 
 def store_tsne_vectors():
     args = parse_args()
-    ner_file_path, qas_file_path  = args.ner_vector_file, args.qas_vector_file
-    limit = 500000
+    ner_file_path, qas_file_path = args.ner_vector_file, args.qas_vector_file
+    limit = args.limit
     save_folder = args.save_folder
+    exp_name = "niter_{}_patience_{}".format(args.n_iter,args.patience)
+    plot_save_path = os.path.join(save_folder,"tsne_visualization_{}.png".format(exp_name))
 
     ner_file_name = os.path.split(ner_file_path)[-1].split(".")[0]
     qas_file_name = os.path.split(qas_file_path)[-1].split(".")[0]
@@ -60,26 +80,26 @@ def store_tsne_vectors():
 
     ner_length = len(ner_feats)
     qas_length = len(qas_feats)
-    vectors = np.vstack([ner_feats,qas_feats])
+    vectors = np.vstack([ner_feats, qas_feats])
     print("Shape of features: {}".format(vectors.shape))
 
-    tsne_vectors = tsne_generation(vectors)
+    tsne_vectors = tsne_generation(vectors, args)
     ner_tsne = tsne_vectors[:ner_length]
     qas_tsne = tsne_vectors[ner_length:]
 
-
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
-    ner_save_path = os.path.join(save_folder,ner_file_name+".hdf5")
-    qas_save_path = os.path.join(save_folder,qas_file_name+".hdf5")
-    with h5py.File(ner_save_path,"w") as h:
-            h["vectors"] = np.array(ner_tsne)
+    ner_save_path = os.path.join(save_folder, ner_file_name + ".hdf5")
+    qas_save_path = os.path.join(save_folder, qas_file_name + ".hdf5")
+    with h5py.File(ner_save_path, "w") as h:
+        h["vectors"] = np.array(ner_tsne)
     with h5py.File(qas_save_path, "w") as h:
         h["vectors"] = np.array(qas_tsne)
-
+    plot_visualization([ner_tsne,qas_tsne],["ner","qas"],plot_save_path)
 
 def main():
     store_tsne_vectors()
+
 
 if __name__ == "__main__":
     main()
