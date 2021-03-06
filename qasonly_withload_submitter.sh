fe66b11ca552befade0ccc243727d400d6e75b4a
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-06 18:22:20 +0900

    tsne

--- qasonly_withload_submitter.sh
+++ qasonly_withload_submitter.sh
@@ -2,7 +2,7 @@
 
 ner_folder=${1}
 
-bioner_names=("linnaeus", "BC5CDR-disease", "BC4CHEMD", "BC5CDR-chem", "NCBI-disease", "JNLPBA", "s800", "BC2GM")
+bioner_names=("linnaeus" "BC5CDR-disease" "BC4CHEMD" "BC5CDR-chem" "NCBI-disease" "JNLPBA" "s800" "BC2GM")
 
 for name in "${bioner_names[@]}"
 do
