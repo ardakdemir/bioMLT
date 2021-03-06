ad75a278914a681460eb1be6590639772e09802f
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-06 18:21:42 +0900

    tsne

--- qasonly_withload_submitter.sh
+++ qasonly_withload_submitter.sh
@@ -4,7 +4,7 @@ ner_folder=${1}
 
 bioner_names=("linnaeus", "BC5CDR-disease", "BC4CHEMD", "BC5CDR-chem", "NCBI-disease", "JNLPBA", "s800", "BC2GM")
 
-for name in $bioner_names
+for name in "${bioner_names[@]}"
 do
   echo $name
 done
