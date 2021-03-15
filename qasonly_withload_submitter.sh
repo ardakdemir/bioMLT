1ce6f4da7e5036ae7995c5a03c6b719d45a376ea
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-15 20:23:20 +0900

    tsne

--- qasonly_withload_submitter.sh
+++ qasonly_withload_submitter.sh
@@ -2,7 +2,7 @@
 
 ner_folder=${1}
 qas_output_dir=${2}
-bioner_names=("linnaeus" "BC5CDR-disease" "BC4CHEMD" "BC5CDR-chem" "NCBI-disease" "JNLPBA" "s800" "BC2GM")
+bioner_names=("linnaeus" "BC5CDR-disease" "BC4CHEMD" "BC5CDR-chem" "NCBI-disease" "JNLPBA" "s800" "BC2GM","All-entities")
 
 for name in "${bioner_names[@]}"
 do
@@ -14,6 +14,24 @@ do
   echo $task_name
   qsub -N $task_name bioMLT/qasonly_withload_submit.sh ${qas_output_dir} $ner_model_path $ner_vocab_path $name
 done
+
+
+for s in 1000 2000 5000 10000 20000
+do
+  for x in bert-instance random
+  do
+    name=$x"_"$s
+    echo $name
+    ner_model_path=$ner_folder"/best_ner_model_on_"$name
+    ner_vocab_path=$ner_folder"/best_ner_model_on_"$name"_vocab"
+    task_name="qasonly_load_"${name}
+    echo $ner_model_path
+    echo $ner_vocab_path
+    echo $task_name
+    qsub -N $task_name bioMLT/qasonly_withload_submit.sh ${qas_output_dir} $ner_model_path $ner_vocab_path $name
+  done
+done
+
 
 
 
