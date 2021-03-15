1c4442b1cd616ede066e837528c1e327d0356add
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-15 20:26:46 +0900

    tsne

--- qasonly_withload_submitter.sh
+++ qasonly_withload_submitter.sh
@@ -2,13 +2,14 @@
 
 ner_folder=${1}
 qas_output_dir=${2}
+exp_pref=${3}
 bioner_names=("linnaeus" "BC5CDR-disease" "BC4CHEMD" "BC5CDR-chem" "NCBI-disease" "JNLPBA" "s800" "BC2GM" "All-entities")
 
 for name in "${bioner_names[@]}"
 do
   ner_model_path=$ner_folder"/best_ner_model_on_"$name
   ner_vocab_path=$ner_folder"/best_ner_model_on_"$name"_vocab"
-  task_name="qasonly_load_"${name}
+  task_name=$exp_pref"qasonly_load_"${name}
   echo $ner_model_path
   echo $ner_vocab_path
   echo $task_name
@@ -24,7 +25,7 @@ do
     echo $name
     ner_model_path=$ner_folder"/best_ner_model_on_"$name
     ner_vocab_path=$ner_folder"/best_ner_model_on_"$name"_vocab"
-    task_name="qasonly_load_"${name}
+    task_name=$exp_pref"qasonly_load_"${name}
     echo $ner_model_path
     echo $ner_vocab_path
     echo $task_name
