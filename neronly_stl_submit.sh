fbc3394bf4388d24b5f42993ed296b89a62d6cab
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-15 22:39:32 +0900

    tsne

--- neronly_stl_submit.sh
+++ neronly_stl_submit.sh
@@ -15,5 +15,5 @@ target_train_file=$folder_path"/ent_train.tsv"
 target_test_file=$folder_path"/ent_test.tsv"
 target_dev_file=$folder_path"/ent_devel.tsv"
 
-singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py --qas_with_ner --crf --mode ner --total_train_steps $total_train_steps --output_dir $output_dir  --num_train_epochs $epoch_num --ner_train_file  $target_train_file  --ner_dev_file $target_dev_file  --ner_test_file $target_test_file  --load_model
+singularity exec --nv ~/singularity/pt-cuda-tf python bioMLT/biomlt_alldata.py  --crf --mode ner --total_train_steps $total_train_steps --output_dir $output_dir  --num_train_epochs $epoch_num --ner_train_file  $target_train_file  --ner_dev_file $target_dev_file  --ner_test_file $target_test_file  --load_model
 echo "Results are stored in "$output_dir
