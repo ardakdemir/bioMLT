cd599517ea05434994dee1152cb8659c33a5d324
Arda Akdemir <ardaakdemir1@gmail.com>
2021-04-09 18:59:51 +0900

    tsne

--- bioasq_pred_submitter.py
+++ bioasq_pred_submitter.py
@@ -7,11 +7,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--test_path", default="../biobert_data/BioASQ-9b/BioASQ-task9bPhaseB-testset2", type=str, required=False,
+        "--test_path", default="../biobert_data/BioASQ-9b/BioASQ-task9bPhaseB-testset3", type=str, required=False,
         help="The path to load the model to continue training."
     )
     parser.add_argument(
-        "--save_folder", default="../bioasq_tb2_2603", type=str, required=False,
+        "--save_folder", default="../bioasq_tb3_0904", type=str, required=False,
         help="The path to save the predictions"
     )
     parser.add_argument(
@@ -26,7 +26,7 @@ def parse_args():
 model_name_map = {"UTokyo_qasonly":"UoT_baseline",
                   "UTokyo_load_All-entities":"UoT_allquestions",
                   "UTokyo_load_BC5CDR-disease":"UoT_multitask_learn",
-                  "UTokyo_load_NCBI-disease":"UoT_bestfac",
+                  "UTokyo_load_NCBI-disease":"UoT_bestfac",#
                   "UTokyo_load_s800":"UoT_bestyesno"}
 
 model_dict = {"UTokyo_qasonly": {"model_path": "../qaswithload_bioberts_1603/best_qas_model_woner"},
